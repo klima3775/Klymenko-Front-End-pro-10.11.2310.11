8e8e6506806fa5f49ed08a0cec0b6534064312eb
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-01-24 00:28:52 +0200

    add coments

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+//кількість стовпців та колонок
 let rows = 10;
 let columns = 10;
 
+//створюємо таблицю
 let table = document.createElement("table");
 
+//числові дані які будуть записані в таблицю
 let counter = 1;
 
+//вкладені цикли для створення табшлиці
 for (let i = 0; i < rows; i++) {
   let row = table.insertRow(i);
 
@@ -12,7 +16,7 @@ for (let i = 0; i < rows; i++) {
     let cell = row.insertCell(j);
     cell.innerHTML = counter;
     counter++;
-
+    //стилі для таблиці
     cell.style.border = "1px solid black";
     cell.style.padding = "8px";
   }
