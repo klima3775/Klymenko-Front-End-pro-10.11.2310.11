afdc9555477453488ad7d95c977b42ded0196d73
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-01-24 03:23:08 +0200

    34 complete

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+const emoji = ["😬", "🥶", "🙈", "🧠", "🤠", "🤠"];
+const votes = new Array(emoji.length).fill(0);
+
+const divSmile = document.getElementById("container");
+
+emoji.forEach(function (smile, index) {
+  const smileElement = document.createElement("span");
+  smileElement.className = "smile";
+  smileElement.textContent = smile;
+
+  smileElement.addEventListener("click", function () {
+    handleSmileyClick(index);
+  });
+
+  divSmile.appendChild(smileElement);
+
+  const count = document.createElement("span");
+  count.textContent = "0";
+  divSmile.appendChild(count);
+});
+
+function handleSmileyClick(index) {
+  votes[index]++;
+  updateVotes();
+}
+
+function updateVotes() {
+  const countElements = document.querySelectorAll(".smile + span");
+  countElements.forEach(function (count, index) {
+    count.textContent = votes[index];
+  });
+}
