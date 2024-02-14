01bcaff84c48c9ceacf498abc309d9d5f1725114
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-02-14 04:53:33 +0200

    add forms and mpdule js in project

---
+++ src/js/categories.js
@@ -0,0 +1,19 @@
+const categories = {
+  Смартфони: [
+    { name: "Poco", price: 5000 },
+    { name: "Xiaomi", price: 6000 },
+    { name: "Samsung", price: 15000 },
+  ],
+  Ноутбуки: [
+    { name: "Hp", price: 20000 },
+    { name: "Asus", price: 40000 },
+    { name: "Acer", price: 27000 },
+  ],
+  Монітори: [
+    { name: "Dell", price: 10000 },
+    { name: "Lenovo", price: 8000 },
+    { name: "MSI", price: 7000 },
+  ],
+};
+
+export default categories;
