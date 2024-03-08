a622007e9afcafe9a2ba215ed1f737430a1511ca
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-03-08 18:52:07 +0200

    completed

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,9 @@ function redirectToSite() {
   let inputUrl = document.getElementById("urlInput").value.trim();
 
   // Перевірка, чи введено http:// або https://
-  if (!inputUrl.startsWith("http://") && !inputUrl.startsWith("https://")) {
+  if (inputUrl.startsWith("http://")) {
+    inputUrl = "https://" + inputUrl.slice(7);
+  } else if (!inputUrl.startsWith("https://")) {
     inputUrl = "https://" + inputUrl;
   }
 
